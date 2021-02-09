f11c6982b0bbdaedce2b2d81093c9aee1dfaaff8
nsktky <nosaka.takuya11@gmail.com>
2021-02-09 22:42:11 +0900

    画像の表示

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class PostBlogModel(models.Model):
     content = models.TextField()
     category = models.ForeignKey(CategoryModel, on_delete=models.PROTECT, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blogimages/', null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -23,6 +24,7 @@ class PostWorkModel(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blogimages/', null=True, blank=True)
 
     def __str__(self):
         return self.title
--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static_root')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media_root')
+
+STATICFILES_DIRS = [str(BASE_DIR / 'static')]
