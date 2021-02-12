2b233a0ff343a9eb4a0e9810c9a208d01cb66e9e
nsktky <nosaka.takuya11@gmail.com>
2021-02-12 20:50:32 +0900

    見た目調整 modelsにlinkフィールド追加

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class PostBlogModel(models.Model):
     category = models.ForeignKey(CategoryModel, on_delete=models.PROTECT, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='blogimages/', null=True, blank=True)
+    link = models.URLField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -25,6 +26,7 @@ class PostWorkModel(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='blogimages/', null=True, blank=True)
+    link = models.URLField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.title
